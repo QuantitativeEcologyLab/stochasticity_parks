9841174d661255ccdc5c0818996bcd29551e41ad
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-07-10 08:43:20 -0700

    fitted gaussian test model and beta model

--- Scripts/006-Canada_bam_mrf.R
+++ Scripts/006-Canada_bam_mrf.R
@@ -14,26 +14,27 @@ d <- readRDS('Data/ndvi-data.rds')
 
 # test spatial smooth only (only using first 100 rasters) ----
 if(FALSE) {
+  d_0 <- filter(d, date <= as.Date('1981-10-01')) %>%
+    mutate(NDVI_scaled = ndvi_to_01(NDVI))
+  
   ecodistricts <-
     st_read("Data/ecodistricts/Canada_Ecodistricts.shp") %>%
     st_geometry() %>%
     st_as_sf() %>%
     st_transform(crs = "EPSG:4326") %>%
     mutate(id = 1:n()) %>%
-    filter(id %in% unique(d$ecodistrict))
+    filter(id %in% unique(d_0$ecodistrict))
   
   plot(ecodistricts)
   plot(rast('Data/ecodistricts/ecodistrict-id.tif'))
   plot(st_geometry(ecodistricts), add = TRUE, lwd = 0.2)
   
-  d$ecodistrict <- as.factor(d$ecodistrict)
+  d_0$ecodistrict <- as.factor(d_0$ecodistrict)
   
   # make a list of neighbors
   nb <- poly2nb(ecodistricts, row.names = ecodistricts$id)
   names(nb) <- attr(nb, "region.id")
-
-  d_0 <- filter(d, date <= as.Date('1981-10-01')) %>%
-    mutate(NDVI_scaled = ndvi_to_01(NDVI))
+  
   nrow(ecodistricts) #' to find max `k` for MRF smooth
   
   # model only using MRF smooth
@@ -82,134 +83,277 @@ hist(fitted(m_sos_0) / fitted(m_sos_0_beta), breaks = 100)
 d <- d %>%
   filter(prop_water < 0.5) %>% # drop pixels that are mostly water
   select(! c(QA, date, ecodistrict)) %>%
-  mutate(across(c(year, doy, pa), .fns = as.integer))
+  mutate(across(c(year, doy, pa), .fns = as.integer)) %>%
+  na.omit()
 gc()
 
-tictoc::tic()
-m <-
-  bam(
-    NDVI ~
-      # global smooths
-      s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
-      s(y, x, bs = 'sos', k = 1000) + # smooth of space
-      s(year, bs = 'cr', k = 10) + # year effect
-      s(doy, bs = 'cc', k = 10) + #seasonal/day of year effect
-      s(elev_m, bs = 'cr', k = 10) + #elevation effect
-      # smooths for difference between in/out of protected areas
-      s(year, by = pa, bs = 'cr', k = 10) +
-      s(doy, by = pa, bs = 'cc', k = 10) +
-      # tensor interaction smooths
-      ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
-      ti(y, x, year, bs = c('sos', 'cr'), d = c(2, 1), k = c(500, 5)) +
-      ti(y, x, doy, bs = c('sos', 'cc'), d = c(2, 1), k = c(500, 5)),
-    family = gaussian(),
-    data = d,
-    method = 'fREML',
-    discrete = TRUE,
-    knots = list(doy = c(0.5, 366.5)),
-    nthreads = 50,
-    control = gam.control(trace = TRUE))
-tictoc::toc()
+# gaussian model (fits in 46 minutes)
+if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')) {
+  m_gaus <- readRDS('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')
+} else {
+  #' `sos` smooths need to be `s(y, x)` to avoid nonsensical plots:
+  bam(NDVI ~ s(x, y, bs = 'sos', k = 100),
+      family = gaussian(),
+      data = slice_sample(d, n = 1e3),
+      discrete = TRUE,
+      method = 'fREML') %>%
+    plot(phi = 0, theta = 180)
+  
+  bam(NDVI ~ s(y, x, bs = 'sos', k = 100),
+      family = gaussian(),
+      data = slice_sample(d, n = 1e3),
+      discrete = TRUE,
+      method = 'fREML') %>%
+    plot(phi = 90, theta = 90)
+  
+  tictoc::tic()
+  m_gaus <-
+    bam(
+      NDVI ~
+        # global smooths
+        s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
+        s(y, x, bs = 'sos', k = 1000) + # smooth of space
+        s(year, bs = 'cr', k = 10) + # year effect
+        s(doy, bs = 'cc', k = 10) + #seasonal/day of year effect
+        s(elev_m, bs = 'cr', k = 10) + #elevation effect
+        # smooths for difference between in/out of protected areas
+        s(year, by = pa, bs = 'cr', k = 10) +
+        s(doy, by = pa, bs = 'cc', k = 10) +
+        # tensor interaction smooths
+        ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
+        ti(y, x, year, bs = c('sos', 'cr'), d = c(2, 1), k = c(500, 5)) +
+        ti(y, x, doy, bs = c('sos', 'cc'), d = c(2, 1), k = c(500, 5)),
+      family = gaussian(),
+      data = d,
+      method = 'fREML',
+      discrete = TRUE,
+      knots = list(doy = c(0.5, 366.5)),
+      nthreads = 50,
+      control = gam.control(trace = TRUE))
+  tictoc::toc()
+  
+  saveRDS(m_gaus, paste0('Models/canada-mean-ndvi-aggr-2-', Sys.Date(),
+                         '-gaussian.rds'))
+  
+  # gratia::draw() causes R to crash because the model is so large
+  png('Figures/full-model-terms-gaussian.png', width = 16, height = 12,
+      units = 'in', res = 300)
+  plot.gam(m_gaus, rug = FALSE, pages = 1, scale = 0, too.far = 0.05,
+           scheme = c(1, 5, rep(1, 5), 3, 5, 5))
+  dev.off()
+}
 
-png('Figures/full-model-terms.png', width= 8, height = 8, units = 'in', res = 300)
-plot(m, pages = 1, scheme = c(1, 5, rep(1, 5), rep(4, 3)),
-     scale = 0, too.far = 0.01)
-dev.off()
+# model summary, since it takes some time to run:
+# Family: gaussian 
+# Link function: identity 
+# 
+# Formula:
+# NDVI ~ s(prop_water, bs = "cr", k = 5) + s(y, x, bs = "sos", 
+#     k = 1000) + s(year, bs = "cr", k = 10) + s(doy, bs = "cc", 
+#     k = 10) + s(elev_m, bs = "cr", k = 10) + s(year, by = pa, 
+#     bs = "cr", k = 10) + s(doy, by = pa, bs = "cc", k = 10) + 
+#     ti(year, doy, bs = c("cr", "cc"), k = c(10, 10)) + ti(y, 
+#     x, year, bs = c("sos", "cr"), d = c(2, 1), k = c(500, 5)) + 
+#     ti(y, x, doy, bs = c("sos", "cc"), d = c(2, 1), k = c(500, 
+#         5))
+# 
+# Parametric coefficients:
+#              Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) 6.049e-02  5.049e-05    1198   <2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Approximate significance of smooth terms:
+#                    edf Ref.df        F p-value    
+# s(prop_water)    3.999      4   428368  <2e-16 ***
+# s(y,x)         998.622    999   242863  <2e-16 ***
+# s(year)          9.000      9  1036659  <2e-16 ***
+# s(doy)           8.000      8 20108834  <2e-16 ***
+# s(elev_m)        9.000      9  1498615  <2e-16 ***
+# s(year):pa       8.997      9     4009  <2e-16 ***
+# s(doy):pa        8.999      9    34315  <2e-16 ***
+# ti(year,doy)    71.994     72   319028  <2e-16 ***
+# ti(year,y,x)  1991.747   1996     6940  <2e-16 ***
+# ti(doy,y,x)   1496.392   1497    94627  <2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Rank: 4613/4614
+# R-sq.(adj) =  0.805   Deviance explained = 80.5%
+# fREML = -4.343e+08  Scale est. = 0.012526  n = 563284823
 
-saveRDS(m, paste0('Models/canada-mean-ndvi-aggr-2-', Sys.Date(), '.rds'))
+# beta model (fits in 11 hours)
+d$NDVI_scaled <- (d$NDVI + 1) / 2
 
-#' *continue editing from here*
+if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')) {
+  m_beta <- readRDS('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')
+} else {
+  tictoc::tic()
+  m_beta <-
+    bam(
+      NDVI_scaled ~
+        # global smooths
+        s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
+        s(y, x, bs = 'sos', k = 1000) + # smooth of space
+        s(year, bs = 'cr', k = 10) + # year effect
+        s(doy, bs = 'cc', k = 10) + #seasonal/day of year effect
+        s(elev_m, bs = 'cr', k = 10) + #elevation effect
+        # smooths for difference between in/out of protected areas
+        s(year, by = pa, bs = 'cr', k = 10) +
+        s(doy, by = pa, bs = 'cc', k = 10) +
+        # tensor interaction smooths
+        ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
+        ti(y, x, year, bs = c('sos', 'cr'), d = c(2, 1), k = c(500, 5)) +
+        ti(y, x, doy, bs = c('sos', 'cc'), d = c(2, 1), k = c(500, 5)),
+      family = betar(link = 'logit'),
+      data = d,
+      method = 'fREML',
+      discrete = TRUE,
+      knots = list(doy = c(0.5, 366.5)),
+      nthreads = 50,
+      control = gam.control(trace = TRUE))
+  tictoc::toc()
+  
+  saveRDS(m_beta, paste0('Models/canada-mean-ndvi-aggr-2-', Sys.Date(),
+                         '-beta.rds'))
+  
+  # gratia::draw() causes R to crash because the model is so large
+  png('Figures/full-model-terms-beta.png', width = 16, height = 12,
+      units = 'in', res = 300)
+  plot.gam(m_beta, rug = FALSE, pages = 1, scale = 0, too.far = 0.05,
+           scheme = c(1, 5, rep(1, 5), 3, 5, 5))
+  dev.off()
+}
 
-canada.mrf.small <- readRDS('Canada/models/ndvi_simplemrf_betals_jun20.rds')
+# model summary, since it takes some time to run:
+# Family: Beta regression(64.537) 
+# Link function: logit 
+# 
+# Formula:
+# NDVI_scaled ~ s(prop_water, bs = "cr", k = 5) + s(y, x, bs = "sos", 
+#     k = 1000) + s(year, bs = "cr", k = 10) + s(doy, bs = "cc", 
+#     k = 10) + s(elev_m, bs = "cr", k = 10) + s(year, by = pa, 
+#     bs = "cr", k = 10) + s(doy, by = pa, bs = "cc", k = 10) + 
+#     ti(year, doy, bs = c("cr", "cc"), k = c(10, 10)) + ti(y, 
+#     x, year, bs = c("sos", "cr"), d = c(2, 1), k = c(500, 5)) + 
+#     ti(y, x, doy, bs = c("sos", "cc"), d = c(2, 1), k = c(500, 
+#         5))
+# 
+# Parametric coefficients:
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) 0.132282   0.000116    1140   <2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Approximate significance of smooth terms:
+#                    edf Ref.df        F p-value    
+# s(prop_water)    3.998      4   377788  <2e-16 ***
+# s(y,x)         998.625    999   222171  <2e-16 ***
+# s(year)          8.999      9   862456  <2e-16 ***
+# s(doy)           7.999      8 19395980  <2e-16 ***
+# s(elev_m)        9.000      9  1381785  <2e-16 ***
+# s(year):pa       8.995      9     3943  <2e-16 ***
+# s(doy):pa        8.998      9    24972  <2e-16 ***
+# ti(year,doy)    71.991     72   354161  <2e-16 ***
+# ti(year,y,x)  1992.701   1996     8631  <2e-16 ***
+# ti(doy,y,x)   1496.417   1497   112841  <2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Rank: 4613/4614
+# R-sq.(adj) =  0.809   Deviance explained = 80.4%
+# fREML = -2.9496e+08  Scale est. = 1         n = 563284823
 
 #predict the new data from the model--------------------------------
+# add residuals to the plot
+if(file.exists('Data_annotated/ndvi-data-with-fitted-and-e.rds')) {
+  d <- readRDS('Data_annotated/ndvi-data-with-fitted-and-e.rds')
+} else {
+  d <- mutate(d,
+              mu_hat = m_beta$fitted.values, # values on [0, 1] scale
+              mu_hat = ndvi_to_11(mu_hat), # map to [-1, 1]
+              e = NDVI - mu_hat)
+  saveRDS(d, 'Data_annotated/ndvi-data-with-fitted-and-e.rds')
+}
 
-library('foreach')
-library('doMC')
-registerDoMC(10) #register how many cores you want to use for this
+# calculate mean + variance spatially from observed values
+if(file.exists('Canada/Data_annotated/summarized-spatial-stats.rds')) {
+  est <- readRDS('Canada/Data_annotated/summarized-spatial-stats.rds')
+} else {
+  est <- d %>%
+    summarize(mu_hat = mean(mu_hat),
+              s2_hat = mean(e^2), #' using `var()` would subtract `mean(e)`
+              .by = c(x, y)) %>%
+    mutate(cv_hat = sqrt(s2_hat) / mu_hat)
+  saveRDS(est, 'Canada/Data_annotated/summarized-spatial-stats.rds')
+}
 
-tic()
-#add the predicted values back to the data frame with all the data
-r <- bind_cols(d,
-               as.data.frame(predict.gam(canada.mrf.small, newdata = d, type = 'response')) %>%
-                 rename(mu = 'predict.gam(canada.mrf.small, newdata = d, type = "response")') %>% #mean
-                 mutate(mu = mu * 2 - 1)) #rescale ndvi
+#additional statistics ----------------------------------------------------
+#' using `lm()` because the large sample size should be enough to justify
+#' the central limit theorem. using `glm()` or `gam()` results in model
+#' estimates that depend on the observed values, unlike for Gaussian models
 
-r <- mutate(r,
-            res = r$NDVI - r$mu)
-
-#calculate mean +  variance spatially
-VAR <- r %>%
-  group_by(x, y) %>%
-  summarize(mean = mean(mu),
-            var = var(res))
-
-#save the resulting datasets
-saveRDS(r, "Canada/Data_annotated/5km_annotated_400MB_predict.rds")
-saveRDS(VAR, "Canada/Data_annotated/5km_400MB_variance.rds")
-
-#additional statistics ------------------------------------------------------
-
+#' *continue editing from here*
 #find average variance and confidence intervals
-model <- lm(var ~ 1, VAR)
+model <- lm(var ~ 1, est)
 confint.lm(model, level = 0.95)
 
 #correlation between mean and variance
-cor.test(x = VAR$mean, y = VAR$var, method = "spearman")
+cor.test(x = est$mean, y = est$var, method = "spearman")
 
 #model variance within and outside of parks
 
-var.park.gamma <- gam(var ~ park, data = VAR,  family = "Gamma")
+var.park.gamma <- gam(var ~ park, data = est,  family = "Gamma")
 plot(var.park.gamma)
 
 #calculate quantiles
 
-quantile(VAR$mean, probs = 0.7) #0.1127625 
-quantile(VAR$var, probs = 0.3, na.rm = TRUE) #0.002437498 
-quantile(VAR$cv, probs = 0.3) 
+quantile(est$mean, probs = 0.7) #0.1127625 
+quantile(est$var, probs = 0.3, na.rm = TRUE) #0.002437498 
+quantile(est$cv, probs = 0.3) 
 
-VAR$mean.quant <- with(VAR, ifelse(mean >= quantile(VAR$mean, probs = 0.7), 1, 0))
-VAR$var.quant <- with(VAR, ifelse(var <= quantile(VAR$var, probs = 0.3, na.rm = TRUE), 1, 0))
-VAR$cv.quant <- with(VAR, ifelse(cv <= quantile(VAR$cv, probs = 0.3), 1, 0))
+est$mean.quant <- with(est, ifelse(mean >= quantile(est$mean, probs = 0.7), 1, 0))
+est$var.quant <- with(est, ifelse(var <= quantile(est$var, probs = 0.3, na.rm = TRUE), 1, 0))
+est$cv.quant <- with(est, ifelse(cv <= quantile(est$cv, probs = 0.3), 1, 0))
 
 #statistics for quantiles
 
 #average NDVI of top mean quantile
-mean(VAR$mean[VAR$mean.quant == 1])
-model <- lm(mean ~ 1, VAR[VAR$mean.quant == 1,])
+mean(est$mean[est$mean.quant == 1])
+model <- lm(mean ~ 1, est[est$mean.quant == 1,])
 confint.lm(model, level = 0.95)
 
 #average variance of bottom variance quantile
-mean(na.omit(VAR$var[VAR$var.quant == 1]))
-model <- lm(na.omit(var) ~ 1, na.omit(VAR[VAR$var.quant == 1,]))
+mean(na.omit(est$var[est$var.quant == 1]))
+model <- lm(na.omit(var) ~ 1, na.omit(est[est$var.quant == 1,]))
 confint.lm(model, level = 0.95)
 
 #percentage of each quantile found in PAs
 
-VAR <- na.omit(VAR)
+est <- na.omit(est)
 
 #amount of high productivity land protected
-VAR %>%
+est %>%
   group_by(park, mean.quant) %>%
-  summarise(percent = 100 * n() / nrow(VAR))
+  summarise(percent = 100 * n() / nrow(est))
 #3.22% of highest productivity land is protected
 
 (3.22*9984670)/100 #sq km of protected land in this quantile
 (26.78*9984670)/100 #sq km of remaining land to protect in this quantile
 
 #amount of low variance land protected
-VAR %>%
+est %>%
   group_by(park, var.quant) %>%
-  summarise(percent = 100 * n() / nrow(VAR))
+  summarise(percent = 100 * n() / nrow(est))
 #3.56% of lowest variance land is protected
 
 (3.56*9984670)/100 #sq km of protected land in this quantile
 (26.44*9984670)/100 #sq km of remaining land to protect in this quantile
 
 #amount of ideal land protected
-VAR %>%
+est %>%
   group_by(park, cv.quant) %>%
-  summarise(percent = 100 * n() / nrow(VAR))
+  summarise(percent = 100 * n() / nrow(est))
 #3.81% of ideal land is protected
 
 (3.81*9984670)/100 #sq km of protected land in this quantile
@@ -217,31 +361,31 @@ VAR %>%
 
 #mean and confidence intervals of specific ecozones
 
-#create table of stats for an ecozone
+#create table of stats for an ecozone (Table S1)
 eco.table <- function(ecozone = 1){
   
   tibble(
     ecozone = ecozone,
     #mean statistics
-    mean = mean(VAR$mean[VAR$layer == ecozone]),
-    upper.mean.confint = confint.lm(lm(mean ~ 1, VAR[VAR$layer == ecozone,]), level = 0.95)[2],
-    lower.mean.confint = confint.lm(lm(mean ~ 1, VAR[VAR$layer == ecozone,]), level = 0.95)[1],
-    mean.parks = mean(na.omit(VAR$mean[c(VAR$layer == ecozone & VAR$park == 1)])),
-    upper.mean.parks.confint = confint.lm(lm(mean ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 1),]), level = 0.95)[2],
-    lower.mean.parks.confint = confint.lm(lm(mean ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 1),]), level = 0.95)[1],
-    mean.out = mean(na.omit(VAR$mean[c(VAR$layer == ecozone & VAR$park == 0)])),
-    upper.mean.out.confint = confint.lm(lm(mean ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 0),]), level = 0.95)[2],
-    lower.mean.out.confint = confint.lm(lm(mean ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 0),]), level = 0.95)[1],
+    mean = mean(est$mean[est$layer == ecozone]),
+    upper.mean.confint = confint.lm(lm(mean ~ 1, est[est$layer == ecozone,]), level = 0.95)[2],
+    lower.mean.confint = confint.lm(lm(mean ~ 1, est[est$layer == ecozone,]), level = 0.95)[1],
+    mean.parks = mean(na.omit(est$mean[c(est$layer == ecozone & est$park == 1)])),
+    upper.mean.parks.confint = confint.lm(lm(mean ~ 1, est[c(est$layer == ecozone & est$park == 1),]), level = 0.95)[2],
+    lower.mean.parks.confint = confint.lm(lm(mean ~ 1, est[c(est$layer == ecozone & est$park == 1),]), level = 0.95)[1],
+    mean.out = mean(na.omit(est$mean[c(est$layer == ecozone & est$park == 0)])),
+    upper.mean.out.confint = confint.lm(lm(mean ~ 1, est[c(est$layer == ecozone & est$park == 0),]), level = 0.95)[2],
+    lower.mean.out.confint = confint.lm(lm(mean ~ 1, est[c(est$layer == ecozone & est$park == 0),]), level = 0.95)[1],
     #variance statistics
-    var = mean(VAR$var[VAR$layer == ecozone]),
-    upper.var.confint = confint.lm(lm(var ~ 1, VAR[VAR$layer == ecozone,]), level = 0.95)[2],
-    lower.var.confint = confint.lm(lm(var ~ 1, VAR[VAR$layer == ecozone,]), level = 0.95)[1],
-    var.parks = mean(na.omit(VAR$var[c(VAR$layer == ecozone & VAR$park == 1)])),
-    upper.var.parks.confint = confint.lm(lm(var ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 1),]), level = 0.95)[2],
-    lower.var.parks.confint =  confint.lm(lm(var ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 1),]), level = 0.95)[1],
-    var.out = mean(na.omit(VAR$var[c(VAR$layer == ecozone & VAR$park == 0)])),
-    upper.var.out.confint = confint.lm(lm(var ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 0),]), level = 0.95)[2],
-    lower.var.out.confint = confint.lm(lm(var ~ 1, VAR[c(VAR$layer == ecozone & VAR$park == 0),]), level = 0.95)[1],
+    var = mean(est$var[est$layer == ecozone]),
+    upper.var.confint = confint.lm(lm(var ~ 1, est[est$layer == ecozone,]), level = 0.95)[2],
+    lower.var.confint = confint.lm(lm(var ~ 1, est[est$layer == ecozone,]), level = 0.95)[1],
+    var.parks = mean(na.omit(est$var[c(est$layer == ecozone & est$park == 1)])),
+    upper.var.parks.confint = confint.lm(lm(var ~ 1, est[c(est$layer == ecozone & est$park == 1),]), level = 0.95)[2],
+    lower.var.parks.confint =  confint.lm(lm(var ~ 1, est[c(est$layer == ecozone & est$park == 1),]), level = 0.95)[1],
+    var.out = mean(na.omit(est$var[c(est$layer == ecozone & est$park == 0)])),
+    upper.var.out.confint = confint.lm(lm(var ~ 1, est[c(est$layer == ecozone & est$park == 0),]), level = 0.95)[2],
+    lower.var.out.confint = confint.lm(lm(var ~ 1, est[c(est$layer == ecozone & est$park == 0),]), level = 0.95)[1],
     
   )
   
