dfa45a67e7399e48188e24431a19d2bfac75189e
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-07-18 10:08:34 -0700

    switched to GAM with `sos` spatial smooth

--- Scripts/006-modeling.R
+++ Scripts/006-modeling.R
@@ -5,7 +5,6 @@ library('mgcv')      # for GAMs
 library('lubridate') # for working with dates
 library('tictoc')    # for estimating time elapsed
 library('ggpubr')    # for data wrangling
-library('spdep')     # to run neighbour analysis
 library('terra')     # for rasters
 source('Functions/scale-ndvi.R') # to scale NDVI to [0, 1] and back
 
@@ -14,6 +13,7 @@ d <- readRDS('Data/ndvi-data.rds')
 
 # test spatial smooth only (only using first 100 rasters) ----
 if(FALSE) {
+  library('spdep')     # to run neighbour analysis
   d_0 <- filter(d, date <= as.Date('1981-10-01')) %>%
     mutate(NDVI_scaled = ndvi_to_01(NDVI))
   
@@ -78,115 +78,21 @@ if(FALSE) {
 
 hist(fitted(m_sos_0) / fitted(m_sos_0_beta), breaks = 100)
 
-# run full model -----------------------------------------------
+# run full beta model ----------------------------------
+# model with sos smooth and k = 1e3 fits in 11 hours
 # not using MRF smooth because it results in visibly discrete areas
 d <- d %>%
-  filter(prop_water < 0.5) %>% # drop pixels that are mostly water
-  select(! c(QA, date, ecodistrict)) %>%
+  filter(prop_water < 0.4) %>% #' `prop_water < 0.5` still gave odd values
+  select(! c(QA, date, ecodistrict)) %>% # reduce dataset size
   mutate(across(c(year, doy, pa), .fns = as.integer)) %>%
-  na.omit()
+  na.omit() # drop any rows with NAs
 gc()
 
-# gaussian model (fits in 46 minutes)
-if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')) {
-  m_gaus <- readRDS('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')
-} else {
-  #' `sos` smooths need to be `s(y, x)` to avoid nonsensical plots:
-  bam(NDVI ~ s(x, y, bs = 'sos', k = 100),
-      family = gaussian(),
-      data = slice_sample(d, n = 1e3),
-      discrete = TRUE,
-      method = 'fREML') %>%
-    plot(phi = 0, theta = 180)
-  
-  bam(NDVI ~ s(y, x, bs = 'sos', k = 100),
-      family = gaussian(),
-      data = slice_sample(d, n = 1e3),
-      discrete = TRUE,
-      method = 'fREML') %>%
-    plot(phi = 90, theta = 90)
-  
-  tictoc::tic()
-  m_gaus <-
-    bam(
-      NDVI ~
-        # global smooths
-        s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
-        s(y, x, bs = 'sos', k = 1000) + # smooth of space
-        s(year, bs = 'cr', k = 10) + # year effect
-        s(doy, bs = 'cc', k = 10) + #seasonal/day of year effect
-        s(elev_m, bs = 'cr', k = 10) + #elevation effect
-        # smooths for difference between in/out of protected areas
-        s(year, by = pa, bs = 'cr', k = 10) +
-        s(doy, by = pa, bs = 'cc', k = 10) +
-        # tensor interaction smooths
-        ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
-        ti(y, x, year, bs = c('sos', 'cr'), d = c(2, 1), k = c(500, 5)) +
-        ti(y, x, doy, bs = c('sos', 'cc'), d = c(2, 1), k = c(500, 5)),
-      family = gaussian(),
-      data = d,
-      method = 'fREML',
-      discrete = TRUE,
-      knots = list(doy = c(0.5, 366.5)),
-      nthreads = 50,
-      control = gam.control(trace = TRUE))
-  tictoc::toc()
-  
-  saveRDS(m_gaus, paste0('Models/canada-mean-ndvi-aggr-2-', Sys.Date(),
-                         '-gaussian.rds'))
-  
-  # gratia::draw() causes R to crash because the model is so large
-  png('Figures/full-model-terms-gaussian.png', width = 16, height = 12,
-      units = 'in', res = 300)
-  plot.gam(m_gaus, rug = FALSE, pages = 1, scale = 0, too.far = 0.05,
-           scheme = c(1, 5, rep(1, 5), 3, 5, 5))
-  dev.off()
-}
+# scale NDVI to fit in [0, 1] interval
+d$NDVI_scaled <- ndvi_to_01(d$NDVI)
 
-# model summary, since it takes some time to run:
-# Family: gaussian 
-# Link function: identity 
-# 
-# Formula:
-# NDVI ~ s(prop_water, bs = "cr", k = 5) + s(y, x, bs = "sos", 
-#     k = 1000) + s(year, bs = "cr", k = 10) + s(doy, bs = "cc", 
-#     k = 10) + s(elev_m, bs = "cr", k = 10) + s(year, by = pa, 
-#     bs = "cr", k = 10) + s(doy, by = pa, bs = "cc", k = 10) + 
-#     ti(year, doy, bs = c("cr", "cc"), k = c(10, 10)) + ti(y, 
-#     x, year, bs = c("sos", "cr"), d = c(2, 1), k = c(500, 5)) + 
-#     ti(y, x, doy, bs = c("sos", "cc"), d = c(2, 1), k = c(500, 
-#         5))
-# 
-# Parametric coefficients:
-#              Estimate Std. Error t value Pr(>|t|)    
-# (Intercept) 6.049e-02  5.049e-05    1198   <2e-16 ***
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-# 
-# Approximate significance of smooth terms:
-#                    edf Ref.df        F p-value    
-# s(prop_water)    3.999      4   428368  <2e-16 ***
-# s(y,x)         998.622    999   242863  <2e-16 ***
-# s(year)          9.000      9  1036659  <2e-16 ***
-# s(doy)           8.000      8 20108834  <2e-16 ***
-# s(elev_m)        9.000      9  1498615  <2e-16 ***
-# s(year):pa       8.997      9     4009  <2e-16 ***
-# s(doy):pa        8.999      9    34315  <2e-16 ***
-# ti(year,doy)    71.994     72   319028  <2e-16 ***
-# ti(year,y,x)  1991.747   1996     6940  <2e-16 ***
-# ti(doy,y,x)   1496.392   1497    94627  <2e-16 ***
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-# 
-# Rank: 4613/4614
-# R-sq.(adj) =  0.805   Deviance explained = 80.5%
-# fREML = -4.343e+08  Scale est. = 0.012526  n = 563284823
-
-# beta model (fits in 11 hours)
-d$NDVI_scaled <- (d$NDVI + 1) / 2
-
-if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')) {
-  m_beta <- readRDS('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')
+if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-beta.rds')) {
+  m_beta <- readRDS('Models/canada-mean-ndvi-aggr-2-2025-07-08-beta.rds')
 } else {
   tictoc::tic()
   m_beta <-
@@ -194,17 +100,17 @@ if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')) {
       NDVI_scaled ~
         # global smooths
         s(prop_water, bs = 'cr', k = 5) + # water biases towards < 0
-        s(y, x, bs = 'sos', k = 1000) + # smooth of space
-        s(year, bs = 'cr', k = 10) + # year effect
-        s(doy, bs = 'cc', k = 10) + #seasonal/day of year effect
-        s(elev_m, bs = 'cr', k = 10) + #elevation effect
+        s(y, x, bs = 'sos', k = 1e3) + # smooth of space
+        s(year, bs = 'cr', k = 12) + # year effect
+        s(doy, bs = 'cc', k = 10) + # seasonal/day of year effect
+        s(elev_m, bs = 'cr', k = 10) + # elevation effect
         # smooths for difference between in/out of protected areas
-        s(year, by = pa, bs = 'cr', k = 10) +
+        s(year, by = pa, bs = 'cr', k = 12) +
         s(doy, by = pa, bs = 'cc', k = 10) +
         # tensor interaction smooths
         ti(year, doy, bs = c('cr', 'cc'), k = c(10, 10)) +
-        ti(y, x, year, bs = c('sos', 'cr'), d = c(2, 1), k = c(500, 5)) +
-        ti(y, x, doy, bs = c('sos', 'cc'), d = c(2, 1), k = c(500, 5)),
+        ti(y, x, year, bs = c('sos', 'cr'), d = c(2, 1), k = c(500, 6)) +
+        ti(y, x, doy, bs = c('sos', 'cc'), d = c(2, 1), k = c(500, 6)),
       family = betar(link = 'logit'),
       data = d,
       method = 'fREML',
@@ -216,16 +122,27 @@ if(file.exists('Models/canada-mean-ndvi-aggr-2-2025-07-08-gaussian.rds')) {
   
   saveRDS(m_beta, paste0('Models/canada-mean-ndvi-aggr-2-', Sys.Date(),
                          '-beta.rds'))
-  
-  # gratia::draw() causes R to crash because the model is so large
+
+  #' `gratia::draw()` causes R to crash
   png('Figures/full-model-terms-beta.png', width = 16, height = 12,
       units = 'in', res = 300)
   plot.gam(m_beta, rug = FALSE, pages = 1, scale = 0, too.far = 0.05,
            scheme = c(1, 5, rep(1, 5), 3, 5, 5))
   dev.off()
+  
+  # check spatial smooth
+  plot(m_beta, select = 2, n2 = 500, scheme = 5)
+  
+  summary(m_beta) # output is included below
+  
+  # fitted and predicted values are different
+  head(fitted(m_beta)) #' equivalent to `head(m_beta$fitted.values)`
+  head(predict(m_beta, newdata = head(d), type = 'response', discrete = F))
+  head(predict(m_beta, newdata = head(d), type = 'response', discrete = T))
 }
 
-# model summary, since it takes some time to run:
+#' *UPDATE*
+# model summary, since it takes a while time to run:
 # Family: Beta regression(64.537) 
 # Link function: logit 
 # 
@@ -270,33 +187,108 @@ if(file.exists('Data_annotated/ndvi-data-with-fitted-and-e.rds')) {
   d <- readRDS('Data_annotated/ndvi-data-with-fitted-and-e.rds')
 } else {
   d <- mutate(d,
-              mu_hat = m_beta$fitted.values, # values on [0, 1] scale
-              mu_hat = ndvi_to_11(mu_hat), # map to [-1, 1]
+              mu_hat = predict(m_beta, newdata = d, type = 'response',
+                               discrete = TRUE) %>%
+                ndvi_to_11(), # map to [-1, 1]
               e = NDVI - mu_hat)
   saveRDS(d, 'Data_annotated/ndvi-data-with-fitted-and-e.rds')
 }
 
 # calculate mean + variance spatially from observed values
-if(file.exists('Canada/Data_annotated/summarized-spatial-stats.rds')) {
-  est <- readRDS('Canada/Data_annotated/summarized-spatial-stats.rds')
+if(file.exists('Data_annotated/summarized-spatial-stats.rds')) {
+  est <- readRDS('Data_annotated/summarized-spatial-stats.rds')
 } else {
   est <- d %>%
     summarize(mu_hat = mean(mu_hat),
               s2_hat = mean(e^2), #' using `var()` would subtract `mean(e)`
               .by = c(x, y)) %>%
-    mutate(cv_hat = sqrt(s2_hat) / mu_hat)
-  saveRDS(est, 'Canada/Data_annotated/summarized-spatial-stats.rds')
+    mutate(cv_hat = sqrt(s2_hat) / mu_hat) %>%
+    as_tibble()
+  saveRDS(est, 'Data_annotated/summarized-spatial-stats.rds')
 }
+
+ggplot(est, aes(y, s2_hat)) +
+  coord_cartesian(ylim = c(0.1)) +
+  geom_hex(bins = 100)
+
+filter(est, y > 70) %>%
+  summarize(s2_hat = mean(s2_hat), .by = y) %>%
+  filter(s2_hat > 0.0075) %>%
+  arrange(y) %>%
+  pull(y)
+
+filter(d, y > 70, doy < 30) %>%
+  summarize(NDVI = mean(NDVI), .by = y) %>%
+  filter(NDVI > 0.0075) %>%
+  arrange(y) %>%
+  pull(y)
+
+# project the estimates to the canada albers projection
+canada_albers <- 'ESRI:102001'
+
+prov <- st_geometry(canadianmaps::PROV) %>%
+  st_transform(canada_albers)
+
+if(file.exists('Data_annotated/summarized-spatial-stats-albers.rds')) {
+  est_proj <- readRDS('Data_annotated/summarized-spatial-stats-albers.rds')
+} else {
+  est_proj <- est %>%
+    rast() %>%
+    `crs<-`('EPSG:4326') %>%
+    project(canada_albers) %>%
+    as.data.frame(xy = TRUE) %>%
+    as_tibble()
+  saveRDS(est_proj, 'Data_annotated/summarized-spatial-stats-albers.rds')
+}
+
+# check limits
+hist(est_proj$mu_hat) # ok
+hist(est_proj$s2_hat, breaks = 30) # clamp to (0, 0.06]
+abline(v = 0.06, col = 'red')
+quantile(est_proj$cv_hat, p = c(0.01, 0.99))
+hist(est_proj$cv_hat, breaks = 5e4, xlim = c(-10, 10)) # clamp to [-3, 3]
+abline(v = c(-3, 3), col = 'red')
+
+est_proj <- est_proj %>%
+  mutate(s2_hat = if_else(s2_hat > 0.06, 0.06, s2_hat),
+         cv_hat = if_else(abs(cv_hat) > 3, sign(cv_hat) * 3, cv_hat))
+
+cowplot::plot_grid(
+  ggplot() +
+    geom_sf(data = prov, fill = 'grey30') +
+    geom_raster(aes(x, y, fill = mu_hat), est_proj) +
+    labs(x = NULL, y = NULL) +
+    scale_fill_viridis_c(expression(widehat(E(NDVI))), option = 'B'),
+  ggplot() +
+    geom_sf(data = prov, fill = 'grey30') +
+    geom_raster(aes(x, y, fill = s2_hat), est_proj) +
+    labs(x = NULL, y = NULL) +
+    scale_fill_viridis_c(expression(widehat(Var(NDVI))), option = 'D',
+                         limits = c(0, NA)),
+  ggplot() +
+    geom_sf(data = prov, fill = 'grey30') +
+    geom_raster(aes(x, y, fill = cv_hat), est_proj) +
+    labs(x = NULL, y = NULL) +
+    scale_fill_distiller(expression(widehat(CV(NDVI))), type = 'div',
+                         palette = 5),
+  nrow = 1)
 
 #additional statistics ----------------------------------------------------
 #' using `lm()` because the large sample size should be enough to justify
 #' the central limit theorem. using `glm()` or `gam()` results in model
 #' estimates that depend on the observed values, unlike for Gaussian models
+brms:::inv_logit(0.132282 + c(0, -1, 1) * 1.96 * 0.000116) %>%
+  ndvi_to_11() %>%
+  round(4)
+
+mutate(est, )
+predict.gam(m_beta, newdata = ., )
 
 #' *continue editing from here*
 #find average variance and confidence intervals
-model <- lm(var ~ 1, est)
-confint.lm(model, level = 0.95)
+m_var <- lm(s2_hat ~ 1, est)
+coef(m_var)
+confint.lm(m_var, level = 0.95)
 
 #correlation between mean and variance
 cor.test(x = est$mean, y = est$var, method = "spearman")
