7f3216a35196a58e73d98030e312a869f3660f51
NoonanM <michael.noonan@ubc.ca>
2025-08-14 15:31:33 -0700

    updated species richness models

--- Scripts/008-richness_modelling.R
+++ Scripts/008-richness_modelling.R
@@ -1,3 +1,14 @@
+#This script explores the relationship between environmental stochasticity
+#and species richness across Canada. It is based on the outputs of the 
+#country-wide modelling in the script entitled 005-modelling.R
+
+
+#-------------------------------------------------------------
+# Workspace and data preparation
+#-------------------------------------------------------------
+
+
+#Load in the necessary packages
 library(terra)
 library(mgcv)
 library(gratia)
@@ -7,16 +18,16 @@ library(ggplot2)
 library(scico)
 
 
-#Import the NDVI rasters
+#Import the rasters describing the mean and variance in NDVI
 mean_ndvi <- rast("Outputs/estimated-mean.tif")
-var_ndvi <- rast("Outputs/mean-squared-residuals.tif")
-cv_ndvi <- rast("Outputs/coefficient-of-variation.tif")
+var_ndvi <- rast("Outputs/estimated-variance.tif")
 
-#Import the species richness data
+#Import the 2024 species richness data obtained from the IUCN
+#https://www.iucnredlist.org/resources/other-spatial-downloads
 richness <- rast("Data/species_richness.tif")
 
 #Convert the species richness raster to a data frame
-richnes_df <- as.data.frame(richness, xy = T, row.names = F)
+richnes_df <- terra::as.data.frame(richness, xy = T, row.names = F)
 row.names(richnes_df) <- NULL
 names(richnes_df)[3] <- "richness"
 #get mean ndvi at coordinates where there is species richness
@@ -25,11 +36,27 @@ richnes_df$mu_hat <- terra::extract(mean_ndvi, richnes_df[,c("x","y")])[,"mu_hat
 richnes_df$var_hat <- terra::extract(var_ndvi, richnes_df[,c("x","y")])[,"s2_hat"]
 
 #Drop extreme values of var_hat
-richnes_df <- richnes_df[richnes_df$var_hat < quantile(richnes_df$var_hat, .99),]
+#richnes_df <- richnes_df[richnes_df$var_hat < quantile(richnes_df$var_hat, .99),]
 
 
-#Fit the gam
-fit <-
+#-------------------------------------------------------------
+# Modelling
+#-------------------------------------------------------------
+
+#Fit a gam predicting richness from mean NDVI alone
+fit_mu <-
+  bam(richness ~ # not scaling because range is in (0, 1)
+        s(mu_hat, k = 5) + # effect of mean ndvi
+        s(x,y, k = 80), # x,y smooth to deal with the spatial autocorrelation
+      
+      family = Gamma(link = "log"),
+      data = richnes_df,
+      discrete = TRUE,
+      method = 'fREML')
+
+
+#Fit a gam predicting richness from both mean and variance in NDVI
+fit_var <-
   bam(richness ~ # not scaling because range is in (0, 1)
         s(mu_hat, k = 5) + # effect of mean ndvi
         s(log(var_hat), k = 5) + # effect of the variance in ndvi
@@ -42,13 +69,31 @@ fit <-
       method = 'fREML')
 
 
-saveRDS(fit, paste0('Outputs/species_richness.rds'))
+saveRDS(fit_var, paste0('Outputs/species_richness_gam.rds'))
 
-summary(fit)
-plot(fit, pages = 1, scheme = c(1, 1, 2, 5))
-draw(fit, n = 250, rug = FALSE) # plot smooths
+summary(fit_var)
+
+gratia::draw(fit_var, n = 250, rug = FALSE, select = "s(mu_hat)") # plot smooths
 
 
+#Does including the variance improve the fit to the data?
+#Note: fit with REML so AIC should be interpreted with some caution,
+#but results are fairly clear
+AIC(fit_mu, fit_var)
+
+#likelihood ratio test
+anova(fit_mu, fit_var, test = "Chisq")
+
+
+#-------------------------------------------------------------
+# Figure generation
+#-------------------------------------------------------------
+
+#----------
+#Panel A effect of mean NDVI on richness
+#----------
+
+#Build a dataframe for generating the predictions for the figure 
 mu_hat <- data.frame(mu_hat = seq(min(richnes_df$mu_hat),
                                   max(richnes_df$mu_hat),
                                   length.out = 500),
@@ -57,19 +102,24 @@ mu_hat <- data.frame(mu_hat = seq(min(richnes_df$mu_hat),
                      y = mean(richnes_df$y)
 )
 
-mu_preds <- predict(fit, newdata = mu_hat, exclude = c("s(log(var_hat))",
-                                                       "ti(mu_hat,log(var_hat))",
-                                                       "s(x,y)"),
+#Predict from the fitted model excluding the terms related to the variance and spatial structure
+mu_preds <- predict(fit_var,
+                    newdata = mu_hat,
+                    terms = s(mu_hat),
+                    #exclude = c("s(log(var_hat))","ti(mu_hat,log(var_hat))","s(x,y)", "(Intercept)"),
                     type = "link",
                     se = TRUE)
 
+#Convert structure for plotting
 mu_hat$richness_hat <- exp(mu_preds$fit)
 mu_hat$richness_min <- exp(mu_preds$fit - 1.96*mu_preds$se.fit)
 mu_hat$richness_max <- exp(mu_preds$fit + 1.96*mu_preds$se.fit)
 
 
+#Generate panel A
 A <- 
   ggplot()+
+  geom_hline(aes(yintercept = 1), col = "grey80", linetype = "dashed") +
   geom_ribbon(data = mu_hat,
               aes(mu_hat,
                   ymin = richness_min,
@@ -82,12 +132,12 @@ A <-
             colour = "darkgreen",
             linewidth = 1) +
   ggtitle("A") +
-  theme_bw() +
+  theme_classic() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
         axis.title.x = element_text(size=10, family = "sans", face = "bold"),
-        axis.text.y = element_text(size=8, family = "sans"),
+        axis.text.y  = element_text(size=10, family = "sans", face = "bold", color = "black"),
         axis.text.x  = element_text(size=10, family = "sans", face = "bold", color = "black"),
         plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
         strip.background = element_blank(),
@@ -101,39 +151,42 @@ A <-
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
-  ylab("Species richness (N)") +
+  ylab("Multiplicative change in species richness") +
   xlab("Mean NDVI") +
-  scale_x_continuous(expand = c(0,0.001)) +
-  scale_y_continuous(expand = c(0,10)) +
-  coord_cartesian(ylim = c(0,2000))
+  scale_x_continuous(expand = c(0,0.005)) +
+  coord_cartesian(ylim = c(-1,20))
 
 
-#Partial effects
-# var_hat <- smooth_estimates(fit, select = "s(log(var_hat))")
-# var_hat <- add_confint(var_hat)
-# var_hat$var_hat <- exp(var_hat$`log(var_hat)`)
+
+#----------
+#Panel B effect of mean NDVI on richness
+#----------
 
 
+#Build a dataframe for generating the predictions for the figure 
 var_hat <- data.frame(var_hat = seq(min(richnes_df$var_hat),
-                                    max(richnes_df$var_hat),
+                                    0.02,
                                     length.out = 1000),
                       mu_hat = mean(richnes_df$mu_hat),
                       x = mean(richnes_df$x),
                       y = mean(richnes_df$y)
 )
 
-var_preds <- predict(fit, newdata = var_hat, exclude = c("s(mu_hat)",
-                                                         "ti(mu_hat,log(var_hat))",
-                                                         "s(x,y)"),
-                     type = "link",
+#Predict from the fitted model excluding the terms related to the mean and spatial structure
+var_preds <- predict(fit_var, newdata = var_hat,
+                     terms = s(log(var_hat)),
+                     #exclude = c("s(mu_hat)", "ti(mu_hat,log(var_hat))", "s(x,y)"),
+                     type = "terms",
                      se = TRUE)
 
+#Convert structure for plotting
 var_hat$richness_hat <- exp(var_preds$fit)
 var_hat$richness_min <- exp(var_preds$fit - 1.96*var_preds$se.fit)
 var_hat$richness_max <- exp(var_preds$fit + 1.96*var_preds$se.fit)
 
 B <- 
   ggplot()+
+  geom_hline(aes(yintercept = 1), col = "grey80", linetype = "dashed") +
   geom_ribbon(data = var_hat,
               aes(var_hat,
                   ymin = richness_min,
@@ -146,12 +199,12 @@ B <-
             colour = "dodgerblue3",
             linewidth = 1) +
   ggtitle("B") +
-  theme_bw() +
+  theme_classic() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
         axis.title.x = element_text(size=10, family = "sans", face = "bold"),
-        axis.text.y = element_text(size=8, family = "sans"),
+        axis.text.y  = element_text(size=10, family = "sans", face = "bold", color = "black"),
         axis.text.x  = element_text(size=10, family = "sans", face = "bold", color = "black"),
         plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
         strip.background = element_blank(),
@@ -165,47 +218,56 @@ B <-
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
-  ylab("Species richness (N)") +
+  ylab("Multiplicative change in species richness") +
   xlab("Variance in NDVI") +
-  #scale_x_continuous(expand = c(0,0.01)) +
-  scale_x_log10(expand = c(0,0.01),
-                breaks = c(0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03),
-                labels = c(0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03)) +
+  scale_x_continuous(expand = c(0,0.0004)) +
+  # scale_x_log10(expand = c(0,0.01),
+  #               breaks = c(0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03),
+  #               labels = c(0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03)) +
   annotation_logticks(sides= "b",
                       short = unit(0.04, "cm"),
                       mid = unit(0.04, "cm"),
                       long = unit(0.1, "cm"),
                       linewidth = 0.2) +
-  scale_y_continuous(expand = c(0,10)) +
-  coord_cartesian(ylim = c(0,2000))
+  coord_cartesian(ylim = c(-1,20))
+
+
+
+
+#----------
+#Panel C interactive effect of mean and variance in NDVI on richness
+#----------
 
 
 var_mu <- as.data.frame(expand_grid(mu_hat = seq(min(richnes_df$mu_hat),
                                                  max(richnes_df$mu_hat), length.out = 100),
                                     var_hat = exp(seq(log(min(richnes_df$var_hat)),
-                                                  log(max(richnes_df$var_hat)), length.out = 100))))
+                                                      log(max(richnes_df$var_hat)), length.out = 100))))
 var_mu$x <- mean(richnes_df$x)
 var_mu$y <- mean(richnes_df$y)
 
-var_preds <- predict(fit, newdata = var_mu, exclude = c("s(mu_hat)",
-                                                         "s(log(var_hat))",
-                                                         "s(x,y)"),
-                     type = "link",
+
+var_mu$too_far <- too_far(var_mu$mu_hat, var_mu$var_hat, richnes_df$mu_hat, richnes_df$var_hat, dist = 0.1)
+
+
+#Predict from the fitted model excluding the spatial structure, and marginal terms
+var_preds <- predict(fit, newdata = var_mu,
+                     terms = c(ti(mu_hat,log(var_hat))),
+                     #exclude = c("s(x,y)", '(Intercept)'),
+                     type = "terms",
                      se = TRUE)
 
 var_mu$richness_hat <- exp(var_preds$fit)
-var_mu$richness_hat[var_mu$richness_hat > 2000] <- 2000
 
-#Partial effects
-# var_mu <- smooth_estimates(fit, select = "ti(mu_hat,log(var_hat))")
-# var_mu$var_hat <- exp(var_mu$`log(var_hat)`)
+#Clamp multiplicative effect at 20 to match axes of A and B
+var_mu[var_mu$richness_hat > 20,"richness_hat"] <- 20
 
 
-fill_mean_lab <- bquote(atop(bold('Species richness (N)')))
+fill_mean_lab <- bquote(atop(bold('Multiplicative change in richness')))
 
 
 C <-
-  ggplot(var_mu) +
+  ggplot(var_mu[!var_mu$too_far,]) +
   geom_raster(aes(mu_hat, var_hat, fill = log(richness_hat))) +
   geom_contour(aes(mu_hat, var_hat, z = log(richness_hat)), color = 'black') +
   scale_x_continuous(expand = c(0,0)) +
@@ -218,30 +280,36 @@ C <-
                       long = unit(0.1, "cm"),
                       linewidth = 0.2) +
   scale_fill_scico(fill_mean_lab,
-                   palette = "batlow",
-                   breaks = log(c(7, 20, 50, 150, 500, 2000)),
-                   labels = c(7, 20, 50, 150, 500, 2000)
-                   ) +
+                   palette = "vik",
+                   direction = -1,
+                   midpoint = 0,
+                   # breaks = c(0, 2, 4, 6, 8),
+                   # labels = c(0, 2, 4, 6, 8),
+                   breaks = log(c(0.001, 0.01, 0.1, 1, 10)),
+                   labels = c(0.001, 0.01, 0.1, 1, 10),
+                   #limits = c(-2, 16)
+  ) +
   ggtitle("C") +
   theme_bw() +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.title.y = element_text(size=10, family = "sans", face = "bold"),
-        axis.title.x = element_text(size=10, family = "sans", face = "bold"),
-        axis.text.y = element_text(size=8, family = "sans"),
-        axis.text.x  = element_text(size=10, family = "sans", face = "bold", color = "black"),
-        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
-        strip.background = element_blank(),
-        strip.text.x = element_blank(),
-        legend.position = "top",
-        # legend.title = element_blank(),
-        # legend.text = element_text(size=5, family = "sans", face = "bold"),
-        # legend.background = element_rect(fill = "transparent"),
-        # legend.key.size = unit(0.3, 'cm'),
-        # legend.spacing.y = unit(0.2, 'cm'),
-        panel.background = element_rect(fill = "transparent"),
-        plot.background = element_rect(fill = "transparent", color = NA),
-        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.y = element_text(size=10, family = "sans", face = "bold"),
+    axis.title.x = element_text(size=10, family = "sans", face = "bold"),
+    axis.text.y = element_text(size=8, family = "sans"),
+    axis.text.x  = element_text(size=8, family = "sans"),
+    plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+    strip.background = element_blank(),
+    strip.text.x = element_blank(),
+    legend.position = "top",
+    # legend.title = element_text(size=5, family = "sans", face = "bold"),
+    legend.title = element_text(size=8, family = "sans", face = "bold"),
+    # legend.background = element_rect(fill = "transparent"),
+    # legend.key.size = unit(0.3, 'cm'),
+    # legend.spacing.y = unit(0.2, 'cm'),
+    panel.background = element_rect(fill = "grey40"),
+    plot.background = element_rect(fill = "transparent", color = NA),
+    plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
   xlab("Mean NDVI") +
   ylab("Variance in NDVI") +
   guides(fill = guide_colorbar(ticks.colour = NA, barwidth = 10,
@@ -254,6 +322,7 @@ cowplot::plot_grid(A, B, C,
                    nrow = 1)
 
 ggsave('Figures/richness_regression.png',
-       width = 6.86, height = 6.86 / 3, units = 'in',
+       width = 6.86, height = (6.86 / 3), units = 'in',
        dpi = 600, bg = 'white', scale = 2)
+
 
