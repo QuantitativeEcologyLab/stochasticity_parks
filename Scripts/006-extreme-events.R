5f3b9bfc498e54f52091d62e4402b59890ed3661
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-08-15 12:49:40 -0700

    moved calculation of extreme events in an if/else

--- Scripts/006-extreme-events.R
+++ Scripts/006-extreme-events.R
@@ -30,10 +30,11 @@ theme_set(
           legend.position.inside = c(0.25, 0.9)))
 
 canada_albers <- 'ESRI:102001'
-canada <- rgeoboundaries::geoboundaries("Canada") %>%
+canada <- rgeoboundaries::geoboundaries('Canada') %>%
   st_geometry() %>%
   st_as_sf() %>%
   st_transform(canada_albers)
+plot(canada)
 
 # rasters of estimated statistics of NDVI
 r_mu <- rast('Outputs/estimated-mean.tif')
@@ -43,70 +44,72 @@ r_cv <- rast('Outputs/coefficient-of-variation.tif')
 plot(r_mu)
 plot(canada, add = TRUE)
 
-# download a raster based on the raster of the spatially aggregated data
-r_0 <- list.files('Data/ndvi-data/AVHRR-Land_v005',
-                  pattern = '.nc', full.names = TRUE) %>%
-  first() %>%
-  rast() %>%
-  terra::aggregate(2)
-
-# using a coarse resolution since it's just a supporting analysis
-dem <- get_elev_raster(r_0, z = 1) %>%
-  rast() %>%
-  crop(., st_transform(canada, crs(.)), mask = TRUE)
-res(dem)
-plot(dem)
-plot(st_transform(canada, crs(dem)), add = TRUE)
-
-#' change the working directory as required by `climatenaR`
-#' this directory should have all the files for climateNA to run
-setwd('ClimateNA_v760')
-writeRaster(dem, 'can-dem.tif')
-plot(rast('can-dem.tif')) # check
-plot(st_transform(canada, 'EPSG:4326'), add = TRUE)
-
-#' convert the can DEM to a csv as required by `climatenaR`
-if(! file.exists('can-dem.csv')) {
-  demToCSV(file = 'can-dem.tif',
-           outdir = '.', # save in current folder
-           srs = NULL) # keep NULL if in lat/long
-  
-  # check the csv
-  print(read.csv('can-dem.csv', nrows = 5))
-  
-  read.csv('can-dem.csv') %>%
-    ggplot() +
-    geom_raster(aes(-long, lat, fill = el)) +
-    coord_sf(crs = 'EPSG:4326')
-}
-
-if(! dir.exists('can-dem')) dir.create('can-dem')
-
-# downloading all possible historical data (2025-08-04)
-map(1901:1980,
-    \(y) {
-      cat(paste0('Downloading estimated historical data for ', y, '...\n'))
-      histClimateNA(file = 'can-dem.csv',
-                    dateR = as.character(y), # year
-                    tFrame = 'M', # monthly averages are the finest scale
-                    exe = 'ClimateNA_v7.60.exe', # must be in wd
-                    outdir = 'can-dem')
-    })
-
-# estimating extremes using a single mean and sd for each pixel to get
-# the number of events outside mean +/- 1.96 * SE
-setwd('..') # go back to main folder
-COLS <- c('Latitude', 'Longitude', 'Elevation', 'Tave01', 'Tave02',
-          'Tave03', 'Tave04', 'Tave05', 'Tave06', 'Tave07', 'Tave08',
-          'Tave09', 'Tave10', 'Tave11', 'Tave12')
-
+# import/create a dataset of the number of months with extreme temperature
 if(file.exists('Data/extreme-temperature-months-1981-2024.rds')) {
   extremes_longlat <- readRDS('Data/extreme-temperature-months-1981-2024.rds')
 } else {
+  # download a raster based on the raster of the spatially aggregated data
+  r_0 <- list.files('Data/ndvi-data/AVHRR-Land_v005',
+                    pattern = '.nc', full.names = TRUE) %>%
+    first() %>%
+    rast() %>%
+    terra::aggregate(2)
+  
+  # using a coarse resolution since it's just a supporting analysis
+  dem <- get_elev_raster(r_0, z = 1) %>%
+    rast() %>%
+    crop(., st_transform(canada, crs(.)), mask = TRUE)
+  res(dem)
+  plot(dem)
+  plot(st_transform(canada, crs(dem)), add = TRUE)
+  
+  #' change the working directory as required by `climatenaR`
+  #' this directory should have all the files for climateNA to run
+  setwd('ClimateNA_v760')
+  writeRaster(dem, 'can-dem.tif')
+  plot(rast('can-dem.tif')) # check
+  plot(st_transform(canada, 'EPSG:4326'), add = TRUE)
+  
+  #' convert the can DEM to a csv as required by `climatenaR`
+  if(! file.exists('can-dem.csv')) {
+    demToCSV(file = 'can-dem.tif',
+             outdir = '.', # save in current folder
+             srs = NULL) # keep NULL if in lat/long
+    
+    # check the csv
+    print(read.csv('can-dem.csv', nrows = 5))
+    
+    read.csv('can-dem.csv') %>%
+      ggplot() +
+      geom_raster(aes(-long, lat, fill = el)) +
+      coord_sf(crs = 'EPSG:4326')
+  }
+  
+  if(! dir.exists('can-dem')) dir.create('can-dem')
+  
+  # downloading all possible historical data (2025-08-04)
+  map(1901:1980,
+      \(y) {
+        cat(paste0('Downloading estimated historical data for ', y, '...\n'))
+        histClimateNA(file = 'can-dem.csv',
+                      dateR = as.character(y), # year
+                      tFrame = 'M', # monthly averages are the finest scale
+                      exe = 'ClimateNA_v7.60.exe', # must be in wd
+                      outdir = 'can-dem')
+      })
+  
+  # estimating extremes using a single mean and sd for each pixel to get
+  # the number of events outside mean +/- 1.96 * SE
+  setwd('..') # go back to main folder
+  
   extremes_longlat <-
     map(1901:2024, function(.y) {
       .fn <- paste0('ClimateNA_v760/can-dem/can-dem_', .y, '.csv')
-      data.table::fread(.fn, na.strings = '-9999', select = c(COLS)) %>%
+      data.table::fread(.fn, na.strings = '-9999',
+                        select = c('Latitude', 'Longitude', 'Elevation',
+                                   'Tave01', 'Tave02', 'Tave03', 'Tave04',
+                                   'Tave05', 'Tave06', 'Tave07', 'Tave08',
+                                   'Tave09', 'Tave10', 'Tave11', 'Tave12')) %>%
         mutate(year = .y) %>%
         return()
     }, .progress = TRUE) %>%
