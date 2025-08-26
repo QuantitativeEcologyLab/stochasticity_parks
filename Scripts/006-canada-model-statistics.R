6c1e079c6130d045dc22f16e5f4caf157e0d2237
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-08-26 12:18:46 -0700

    updated script with reviewed results

--- Scripts/006-canada-model-statistics.R
+++ Scripts/006-canada-model-statistics.R
@@ -4,35 +4,37 @@ library('mgcv')    # for GAMs
 library('tidyr')   # for data wrangling
 library('purrr')   # for functional programming
 library('ggplot2') # for fancy plots
+library('terra')   # for rasters
+
+options(digits = 4) # change rounding to be 4 digits
 
 canada_albers <- 'ESRI:102001'
-prov <- st_transform(st_geometry(canadianmaps::PROV), canada_albers)
-est_albers <- readRDS('Data_annotated/summarized-spatial-stats-albers.rds')
-
-#' find average variance and confidence intervals
-#' using `lm()` because the large sample size should be enough to justify
-#' the central limit theorem.
-m_var <- lm(s2_hat ~ 1, est_albers) # using equal-area projected data
-coef(m_var)
-confint.lm(m_var, level = 0.95)
+prov <- rgeoboundaries::geoboundaries("Canada") %>%
+  st_transform(canada_albers)
+est_albers <-
+  readRDS('Data_annotated/summarized-spatial-stats-albers.rds') %>%
+  mutate(cv_hat = if_else(cv_hat < 0, Inf, cv_hat),
+         richness = extract(rast('Data/species_richness.tif'),
+                            tibble(x, y))[, 2],
+         extremes = extract(rast('Outputs/n-extreme-months-1981-2024.tif'),
+                            tibble(x, y))[, 2])
 
 # correlation between mean and variance
 # p-value is < 2e-16 even for the first 1e3 values only
 cor.test(x = est_albers$mu_hat, y = est_albers$s2_hat, method = "spearman")
 
-# model variance within and outside of parks
-m_var_pa <- lm(s2_hat ~ pa, data = mutate(est_albers, pa = factor(pa)))
-summary(m_var_pa)
-
 #calculate quantiles
 quantile(est_albers$mu_hat, probs = 0.7)
 quantile(est_albers$s2_hat, probs = 0.3, na.rm = TRUE)
-quantile(est_albers$cv_hat, probs = 0.3) 
+quantile(est_albers$cv_hat, probs = 0.3)
 
-est_albers <- mutate(est_albers,
-                     mu_hat_q = mu_hat >= quantile(mu_hat, probs = 0.7),
-                     s2_hat_q = s2_hat <= quantile(s2_hat, probs = 0.3),
-                     cv_hat_q = cv_hat <= quantile(cv_hat, probs = 0.3))
+# find cells within the quantiles
+est_albers <- est_albers %>%
+  mutate(mu_hat_q = mu_hat >= quantile(mu_hat, probs = 0.7),
+         s2_hat_q = s2_hat <= quantile(s2_hat, probs = 0.3),
+         cv_hat_q = cv_hat <= quantile(cv_hat, probs = 0.3),
+         rich_q = richness >= quantile(richness, probs = 0.7, na.rm = TRUE),
+         extr_q = extremes <= quantile(extremes, probs = 0.3, na.rm = TRUE))
 
 #statistics for quantiles ----
 #average NDVI of top mean quantile
@@ -45,6 +47,11 @@ m_s2_q <- lm(s2_hat ~ 1, filter(est_albers, s2_hat_q))
 coef(m_s2_q)['(Intercept)']
 confint.lm(m_s2_q, level = 0.95)
 
+# average cv of bottom cv quantile
+m_cv_q <- lm(cv_hat ~ 1, filter(est_albers, cv_hat_q))
+coef(m_cv_q)['(Intercept)']
+confint.lm(m_cv_q, level = 0.95)
+
 # find how much of each "good" quantile is protected ----
 # i.e. what percentage of each good quantile is in a PAs? 
 # total area of canada
@@ -54,49 +61,22 @@ prov_area_km2 <- prov %>%
   `units<-`('km^2') %>%
   as.numeric()
 
-# area corresponding to each "best" quantile (equivalent across all 3)
+# 30% of the area of canada
 q_area_km2 <- prov_area_km2 * mean(est_albers$mu_hat_q)
 
+# area corresponding to each "best" quantile (equivalent across all 3)
 est_albers %>%
-  select(pa, mu_hat_q, s2_hat_q, cv_hat_q) %>%
+  select(pa, mu_hat_q, s2_hat_q, cv_hat_q, rich_q, extr_q) %>%
   pivot_longer(! pa, names_to = 'parameter', values_to = 'in_q') %>%
-  filter(in_q) %>% # filter to inside best quantiles only
-  select(! in_q) %>% # no longer necessary
   # find approximate proportion of quantile that is in a PA
-  summarize(proportion = mean(pa, na.rm = TRUE), .by = parameter) %>%
+  summarize(proportion = mean(pa, na.rm = TRUE),
+            .by = c(parameter, in_q)) %>%
   mutate(percentage = proportion * 100,
-         area_km = proportion * q_area_km2)
-
-# mean and confidence intervals of specific ecozones (Table S1) ----
-options(scipen = 10) # to prevent scientific notation in the table
-
-# create the table of stats for an ecozone
-ecozone_summary <-
-  expand_grid(Ecozone = unique(est_albers$ecozone),
-              Subset = c('Overall', 'Inside protected areas', 'Outside protected areas')) %>%
-  mutate(output = map2(Ecozone, Subset, function(.eco, .pa) {
-    # subset the data
-    .d <- filter(est_albers, ecozone == .eco)
-    if(.pa == 'Inside protected areas') .d <- filter(.d, pa == 1)
-    if(.pa == 'Outside protected areas') .d <- filter(.d, pa == 0)
-    
-    # fit the models
-    m_mu <- lm(mu_hat ~ 1, .d)
-    m_s2 <- lm(s2_hat ~ 1, .d)
-    
-    make_ci <- function(x) {
-      paste0('(', round(x[1], 4), ', ', round(x[2], 4), ')')
-    }
-    
-    # extract the estimates and CIs of mean and variance
-    tibble(mu_hat_est = round(coef(m_mu)['(Intercept)'], 4),
-           mu_hat_95_ci = make_ci(confint.lm(m_mu, level = 0.95)),
-           s2_hat_est = coef(m_s2)['(Intercept)'],
-           s2_hat_95_ci = make_ci(confint.lm(m_s2, level = 0.95))) %>%
-      return()
-  })) %>%
-  unnest(output)
-
-print(ecozone_summary, n = 15 * 3)
-
-readr::write_csv(ecozone_summary, 'Outputs/ecozone-summary-statistics.csv')
+         ref_area = if_else(in_q, q_area_km2, prov_area_km2 - q_area_km2),
+         protected_km2 = proportion * ref_area,
+         unprotected_km2 = ref_area - protected_km2,
+         parameter = factor(parameter, levels = c('mu_hat_q', 's2_hat_q',
+                                                  'cv_hat_q', 'rich_q',
+                                                  'extr_q'))) %>%
+  arrange(parameter, in_q) %>%
+  filter(! is.na(in_q)) # missing some richness and extreme values
